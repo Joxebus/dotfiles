cf455a808ff0e44736256305c756ff38acd88452
Rubén Salvador García San Juan <rssanjuan2704@gmail.com>
2017-03-16 10:50:24 -0400

    [MDF] Add bower support.

--- proxy-change.sh
+++ proxy-change.sh
@@ -9,10 +9,12 @@ elif [ "$1" = "local" ]; then
 	npm config set proxy=http://localhost:3131
 	npm config set https-proxy=http://localhost:3131
 	npm config set registry "http://localhost:8085/repository/localnpm"
+    sed -i  -e "s/nexus.prod.uci.cu\/repository\/bower-proxy/localhost:8085\/repository\/bowerlocal/g" $HOME/.bowerrc 
 elif [ "$1" = "university" ]; then
 	npm config set proxy=http://localhost:3131
 	npm config set https-proxy=http://localhost:3131
 	npm config set registry "http://nexus.prod.uci.cu/repository/npm-proxy"
+    sed -i -e "s/localhost:8085\/repository\/bowerlocal/nexus.prod.uci.cu\/repository\/bower-proxy/g" $HOME/.bowerrc
 elif [ "$1" = "-h" ]; then
 	echo 'Usage proxy-change option'
 	echo 'options: [internet|local|university]'
