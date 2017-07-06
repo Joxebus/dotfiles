07b4708f0deaab2cdcff48c97e0cf7578d965f14
Rubén Salvador García San Juan <rssanjuan2704@gmail.com>
2017-07-06 17:14:29 -0400

    [MDF] Install.sh tunes.

--- bash_prompt/install.sh
+++ bash_prompt/install.sh
@@ -2,7 +2,11 @@ ln -s `pwd`/prompt_command ~/.prompt_command
 ln -s `pwd`/bash_colors ~/.bash_colors
 ln -s `pwd`/gitprompt ~/gitprompt
 ln -s `pwd`/define-colors ~/define-colors
+ln -s `pwd`/.bash_functions ~/.bash_functions
+ln -s `pwd`/.bash_aliases ~/.bash_aliases
 
+echo "source ~/.bash_aliases" >> ~/.bashrc
+echo "source ~/.bash_functions" >> ~/.bashrc
 echo "source ~/.bash_colors" >> ~/.bashrc
 echo "source ~/.prompt_command" >> ~/.bashrc
 echo 'export PROMPT_COMMAND="prompt_command"' >> ~/.bashrc
