966b6954fe5948a980caaa3b2ce3a3231602ec45
Rubén Salvador García San Juan <rssanjuan2704@gmail.com>
2017-09-09 20:07:40 -0400

    [MDF] Prompt installation.

--- bash_prompt/install.sh
+++ bash_prompt/install.sh
@@ -4,7 +4,9 @@ ln -s `pwd`/gitprompt ~/.gitprompt
 ln -s `pwd`/define-colors ~/.define-colors
 ln -s `pwd`/bash_functions ~/.bash_functions
 ln -s `pwd`/bash_aliases ~/.bash_aliases
+ln -s `pwd`/gitstatus ~/gitstatus
 
+echo "__GIT_PROMPT_DIR='/home/sigfried'" >> ~/.bashrc
 echo "source ~/.bash_aliases" >> ~/.bashrc
 echo "source ~/.bash_functions" >> ~/.bashrc
 echo "source ~/.bash_colors" >> ~/.bashrc
