072d18fc8b4a82192c1b6283d3097ae7151581e9
Rubén Salvador García San Juan <rssanjuan2704@gmail.com>
2017-10-07 08:15:51 -0400

    [UPD] Install script.

--- bash_prompt/install.sh
+++ bash_prompt/install.sh
@@ -6,7 +6,7 @@ ln -s `pwd`/bash_functions ~/.bash_functions
 ln -s `pwd`/bash_aliases ~/.bash_aliases
 ln -s `pwd`/gitstatus ~/gitstatus
 
-echo "__GIT_PROMPT_DIR='/home/sigfried'" >> ~/.bashrc
+echo "__GIT_PROMPT_DIR='$(pwd)'" >> ~/.bashrc
 echo "source ~/.bash_aliases" >> ~/.bashrc
 echo "source ~/.bash_functions" >> ~/.bashrc
 echo "source ~/.bash_colors" >> ~/.bashrc
