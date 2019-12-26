6c0c9bce2047b4d3c7bc534cc5b0349c704a612f
sigfried <rssanjuan2704@gmail.com>
2019-12-26 13:13:03 -0600

    [UPD] Environment variables for Go

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,8 +2,10 @@ export VISUAL=vim
 export EDITOR=vim
 export SUDO_EDITOR=vim
 
+export GOPATH=$PATH:/$HOME/go/bin
+
 export LANG=en_US.UTF-8
-export PATH=$PATH:/$HOME/.npmglobal/bin/
+export PATH=$PATH:/$HOME/.npmglobal/bin/:$GOPATH
 
 #Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
