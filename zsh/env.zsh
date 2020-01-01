cb7b686a62e118c61b9ad0837df9c86462b15315
sigfried <rssanjuan2704@gmail.com>
2019-12-31 20:04:33 -0600

    [REF] Zsh env file

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,9 +3,11 @@ export EDITOR=vim
 export SUDO_EDITOR=vim
 
 export GOPATH=$PATH:/$HOME/go/bin
+export RUBYPATH=$PATH:/$HOME/.gem/ruby/2.6.0/bin
+export NODEPATH=$PATH:/$HOME/.npmglobal/bin
 
 export LANG=en_US.UTF-8
-export PATH=$PATH:/$HOME/.npmglobal/bin/:$GOPATH
+export PATH=$PATH:$NODEPATH:$GOPATH:$RUBYPATH
 
 #Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
