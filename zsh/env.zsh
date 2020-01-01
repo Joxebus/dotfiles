a0af23a53c36ad18b643cd23caf9aef8c05f02ed
sigfried <rssanjuan2704@gmail.com>
2020-01-01 16:59:06 -0600

    [REF] make NeoVim the default editor & modified variables name.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,21 +1,26 @@
-export VISUAL=vim
-export EDITOR=vim
-export SUDO_EDITOR=vim
+export VISUAL=nvim
+export EDITOR=nvim
+export SUDO_EDITOR=nvim
 
-export GOPATH=$PATH:/$HOME/go/bin
-export RUBYPATH=$PATH:/$HOME/.gem/ruby/2.6.0/bin
-export NODEPATH=$PATH:/$HOME/.npmglobal/bin
+export GO_PATH=$PATH:/$HOME/go/bin
+export RUBY_PATH=$PATH:/$HOME/.gem/ruby/2.6.0/bin
+export NODE_PATH=$PATH:/$HOME/.npmglobal/bin
+export YARN_PATH=$PATH:/$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin
 
 export LANG=en_US.UTF-8
-export PATH=$PATH:$NODEPATH:$GOPATH:$RUBYPATH
+export PATH=$PATH:$NODE_PATH:$GO_PATH:$RUBY_PATH:$YARN_PATH
 
 #Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='vim'
+  export EDITOR='nvim'
 else
-  export EDITOR='vim'
+  export EDITOR='nvim'
 fi
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+# Colorls
 source $(dirname $(gem which colorls))/tab_complete.sh
+
+# Init starship prompt
 eval "$(starship init zsh)"
